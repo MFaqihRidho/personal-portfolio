a46e36429974ab3cc0a426780b13af4df176fdf8
MFaqihRidho <faqih.fr5@gmail.com>
2022-03-12 13:39:31 +0700

    adding certificate

---
+++ src/components/border.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Border(props) {
+    return (
+        <div className="relative hover:grayscale-0 transition-all duration-300 grayscale flex items-center justify-center border-[7px] w-full h-full">
+            <div className="w-full bg-white flex justify-between h-6 absolute right-0 left-0 top-0 border-b-[7px]">
+                <div className="flex flex-row gap-1">
+                    <div className="w-4 h-4 bg-black rounded-full "></div>
+                    <div className="w-4 h-4 bg-black rounded-full "></div>
+                    <div className="w-4 h-4 bg-black rounded-full "></div>
+                </div>
+                <div className="w-10 h-full border-l-8"></div>
+            </div>
+            {props.children}
+        </div>
+    );
+}
+
+export default Border;
--- src/components/certificate.js
+++ src/components/certificate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Border from "./border";
 
 function Certificate() {
     return (
@@ -11,7 +12,7 @@ function Certificate() {
                 <path
                     fill="#FCA311"
                     fill-opacity="1"
-                    d="M0,128L80,128C160,128,320,128,480,149.3C640,171,800,213,960,213.3C1120,213,1280,171,1360,149.3L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+                    d="M0,128L80,149.3C160,171,320,213,480,224C640,235,800,213,960,202.7C1120,192,1280,192,1360,192L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
                 ></path>
             </svg>
             <div className="w-full py-10 text-black min-h-fit bg-main">
@@ -19,8 +20,149 @@ function Certificate() {
                     <h1 className="mt-10 ml-5 text-3xl font-bold pb-7 md:text-5xl">
                         My Certificate
                     </h1>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.dicoding.com/certificates/L4PQ37RW4PO1"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://i.postimg.cc/43GrJtg4/sertifikat-course-163-1265221-211221131106-1.png"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.freecodecamp.org/certification/faqih_ridho/responsive-web-design"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://i.postimg.cc/9M9202kC/image-2022-03-12-130929.png"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.freecodecamp.org/certification/faqih_ridho/front-end-development-libraries"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://i.postimg.cc/KvMTb3fH/image-2022-03-12-131553.png"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.sololearn.com/Certificate/1014-23861767/jpg/"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://www.sololearn.com/Certificate/1014-23861767/jpg/"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.sololearn.com/Certificate/1023-23861767/jpg/"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://www.sololearn.com/Certificate/1023-23861767/jpg"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.sololearn.com/certificates/course/en/23861767/1024/landscape/png/"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://www.sololearn.com/certificates/course/en/23861767/1024/landscape/png"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.sololearn.com/Certificate/1162-23861767/jpg/"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://www.sololearn.com/Certificate/1162-23861767/jpg"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                        <div className="flex flex-col gap-3 text-center">
+                            <Border
+                                children={
+                                    <a
+                                        target={"_blank"}
+                                        href="https://www.sololearn.com/Certificate/1097-23861767/jpg/"
+                                    >
+                                        <img
+                                            className="xl:h-80 lg:h-64 h-28 md:h-44 w-full md:w-90 object-cover"
+                                            src="https://www.sololearn.com/Certificate/1097-23861767/jpg"
+                                            alt=""
+                                        />
+                                    </a>
+                                }
+                            ></Border>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <svg
+                className="-mt-1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1440 320"
+            >
+                <path
+                    fill="#FCA311"
+                    fill-opacity="1"
+                    d="M0,96L120,90.7C240,85,480,75,720,90.7C960,107,1200,149,1320,170.7L1440,192L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+                ></path>
+            </svg>
         </div>
     );
 }
